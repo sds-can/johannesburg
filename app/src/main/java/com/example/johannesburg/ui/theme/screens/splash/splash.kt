f47fb0f21a14b50b8f8834d2804ab3bc477685fc
sds-can <shinedreamsmile20@gmail.com>
2025-03-10 23:24:40 +0300

    update with splash implementation

---
+++ app/src/main/java/com/example/johannesburg/ui/theme/screens/splash/splash.kt
@@ -0,0 +1,116 @@
+package com.example.johannesburg.ui.theme.screens.splash
+
+
+import android.net.Uri
+
+import android.os.Bundle
+
+import android.widget.VideoView
+
+import androidx.activity.ComponentActivity
+
+import androidx.activity.compose.setContent
+
+import androidx.compose.foundation.layout.Box
+
+import androidx.compose.foundation.layout.fillMaxSize
+
+import androidx.compose.ui.Alignment
+
+import androidx.compose.ui.Modifier
+
+import androidx.compose.ui.platform.LocalContext
+
+import androidx.compose.ui.viewinterop.AndroidView
+
+import androidx.core.content.res.ResourcesCompat
+
+import androidx.lifecycle.lifecycleScope
+
+
+
+import kotlinx.coroutines.delay
+
+import kotlinx.coroutines.launch
+
+import android.content.Intent
+
+import androidx.compose.runtime.Composable
+
+import androidx.compose.ui.tooling.preview.Preview
+
+import com.example.johannesburg.R
+
+import com.example.johannesburg.ui.theme.screens.registration.RegisterActivity
+
+
+
+
+
+class SplashActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+
+        setContent {
+
+            SplashScreen {
+
+                startActivity(Intent(this, RegisterActivity::class.java))
+
+                finish()
+
+            }
+
+        }
+
+    }
+
+}
+
+
+
+@Composable
+
+fun SplashScreen(onVideoEnd: () -> Unit) {
+
+    val context = LocalContext.current
+
+
+
+    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+
+        AndroidView(factory = { ctx ->
+
+            VideoView(ctx).apply {
+
+                setVideoURI(Uri.parse("android.resource://${context.packageName}/${R.raw.splash_screen}"))
+
+                setOnCompletionListener { onVideoEnd() }
+
+                start()
+
+            }
+
+        })
+
+    }
+
+}
+
+
+
+@Preview
+
+@Composable
+
+fun PreviewSplashScreen() {
+
+    SplashScreen(
+
+        onVideoEnd = TODO()
+
+    )
+
+}
